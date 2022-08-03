3637d4e3803cbb9a4b23eba3cd6bdb5d72c3ed79
NebulaAnish <50112251+NebulaAnish@users.noreply.github.com>
2022-08-03 19:19:05 +0545

    Update Keyboard control.py

--- Keyboard control.py
+++ Keyboard control.py
@@ -4,7 +4,9 @@ x,y = pyautogui.position()
 pyautogui.click(600,600)
 pyautogui.typewrite(['a','b','left','left','d','right','right','s'],interval=0.1)
 
+
 #pyautogui.press("f1")
 #pyautogui.hotkey("ctrl","o")
+#pyautogui
 #pyautogui.typewrite()
 #pyautogui.KEYBOARD_KEYS
