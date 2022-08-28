e09adb3aff6cd42591b327c69c7f7e74b87c6d14
NebulaAnish <50112251+NebulaAnish@users.noreply.github.com>
2022-08-28 22:17:18 +0545

    Update clinicname_scraper.py

--- clinicname_scraper.py
+++ clinicname_scraper.py
@@ -1,9 +1,14 @@
+#test clinic id 12695
+import requests
+from bs4 import BeautifulSoup
 def get_clinic(clinic_id):
     
-    url = "https://12695.portal.athenahealth.com/"
+    url = f"https://{clinic_id}.portal.athenahealth.com/"
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html,"lxml")
     h1_list = soup.find_all('h1')
     clinic_name= h1_list[-1].text.strip()
     return clinic_name
+
+
